fd167d9cf126e14d3d63cf3a0364c39c35f161b8
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-02-16 21:36:23 -0700

    Configuración actualizada.

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.ts',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-});
+  plugins: [
+    laravel({
+      input: 'resources/js/app.ts',
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+        compilerOptions: {
+          isCustomElement: tag => tag.startsWith('md-'),
+        },
+      },
+    }),
+  ],
+})
