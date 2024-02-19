677b743be7e0f12974aadd1dcaddf86025a45ae4
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-02-19 14:45:40 -0700

    App template actualizado

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,21 +1,40 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html
+  class="h-svh"
+  lang="{{ str_replace('_', '-', app()->getLocale()) }}"
+>
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+<head>
+  <meta charset="utf-8">
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1"
+  >
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+  <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @routes
-        @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+  <!-- Fonts -->
+  <link
+    href="https://fonts.bunny.net"
+    rel="preconnect"
+  >
+  <link
+    href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap"
+    rel="stylesheet"
+  />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:FILL@0..1"
+    rel="stylesheet"
+  >
+
+  <!-- Scripts -->
+  @routes
+  @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
+  @inertiaHead
+</head>
+
+<body class='h-svh bg-light-background font-sans antialiased dark:bg-dark-background'>
+  @inertia
+</body>
+
 </html>
