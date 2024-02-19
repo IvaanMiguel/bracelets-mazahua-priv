d56a3c40b7067e5ec9e885bcaa903c961f5949e2
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-02-19 14:46:29 -0700

    Componente Icon creado.

---
+++ resources/js/Components/Icon.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import '@material/web/icon/icon'
+
+withDefaults(
+  defineProps<{
+    filled?: boolean
+    size?: string
+  }>(),
+  {
+    filled: false,
+    size: '24px',
+  }
+)
+</script>
+
+<template>
+  <md-icon :filled="filled">
+    <slot />
+  </md-icon>
+</template>
+
+<style scoped>
+md-icon {
+  --md-icon-font: 'Material Symbols Rounded';
+  --md-icon-size: v-bind(size);
+}
+md-icon[filled='true'] {
+  font-variation-settings: 'FILL' 1;
+}
+</style>
