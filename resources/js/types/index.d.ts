659edb58a93f5ea75c3fbe38d6dc45833c445de9
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-02-19 15:03:14 -0700

    Index types actualizados.

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,12 +1,20 @@
 export interface User {
-    id: number;
-    name: string;
-    email: string;
-    email_verified_at: string;
+  username: string
+  email: string
+  email_verified_at: string
 }
 
-export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
-    auth: {
-        user: User;
-    };
-};
+export type PageProps<
+  T extends Record<string, unknown> = Record<string, unknown>,
+> = T & {
+  auth: {
+    user: User
+  }
+}
+
+export interface MenuButton {
+  icon: string
+  label: string
+  href: string
+  component: string
+}
