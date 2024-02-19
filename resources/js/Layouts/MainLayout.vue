1686651f2afef4b75368b4d147d9c88562a8d27f
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-02-19 14:46:59 -0700

    Main layour creado.

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Header from './Partials/Header.vue'
+import LateralMenu from './Partials/Menu/LateralMenu.vue'
+</script>
+
+<template>
+  <div class="flex h-svh">
+    <LateralMenu />
+    <div class="flex flex-1 flex-col">
+      <Header />
+      <main class="h-svh overflow-y-auto p-4">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
