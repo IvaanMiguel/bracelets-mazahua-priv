b7ec34dfeb2d1d038e58f2733df2c603f1cb3e12
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-02-19 14:46:49 -0700

    Componente modal recreado.

--- resources/js/Components/Modal.vue
+++ resources/js/Components/Modal.vue
@@ -1,96 +1,55 @@
 <script setup lang="ts">
-import { computed, onMounted, onUnmounted, watch } from 'vue';
+import '@material/web/dialog/dialog'
+import { MdDialog } from '@material/web/dialog/dialog'
+
+import { onUnmounted, onMounted, ref } from 'vue'
 
 const props = withDefaults(
-    defineProps<{
-        show?: boolean;
-        maxWidth?: 'sm' | 'md' | 'lg' | 'xl' | '2xl';
-        closeable?: boolean;
-    }>(),
-    {
-        show: false,
-        maxWidth: '2xl',
-        closeable: true,
-    }
-);
+  defineProps<{
+    id: string
+    notCancellable?: boolean
+  }>(),
+  {
+    notCancellable: false,
+  }
+)
 
-const emit = defineEmits(['close']);
+const modal = ref<MdDialog | null>(null)
 
-watch(
-    () => props.show,
-    () => {
-        if (props.show) {
-            document.body.style.overflow = 'hidden';
-        } else {
-            document.body.style.overflow = 'visible';
-        }
-    }
-);
+const preventCancel = (e: Event) => e.preventDefault()
 
-const close = () => {
-    if (props.closeable) {
-        emit('close');
-    }
-};
-
-const closeOnEscape = (e: KeyboardEvent) => {
-    if (e.key === 'Escape' && props.show) {
-        close();
-    }
-};
-
-onMounted(() => document.addEventListener('keydown', closeOnEscape));
+onMounted(() => {
+  if (props.notCancellable) {
+    modal.value?.addEventListener('cancel', preventCancel)
+  }
+})
 
 onUnmounted(() => {
-    document.removeEventListener('keydown', closeOnEscape);
-    document.body.style.overflow = 'visible';
-});
-
-const maxWidthClass = computed(() => {
-    return {
-        sm: 'sm:max-w-sm',
-        md: 'sm:max-w-md',
-        lg: 'sm:max-w-lg',
-        xl: 'sm:max-w-xl',
-        '2xl': 'sm:max-w-2xl',
-    }[props.maxWidth];
-});
+  modal.value?.removeEventListener('cancel', preventCancel)
+})
 </script>
 
 <template>
-    <Teleport to="body">
-        <Transition leave-active-class="duration-200">
-            <div v-show="show" class="fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50" scroll-region>
-                <Transition
-                    enter-active-class="ease-out duration-300"
-                    enter-from-class="opacity-0"
-                    enter-to-class="opacity-100"
-                    leave-active-class="ease-in duration-200"
-                    leave-from-class="opacity-100"
-                    leave-to-class="opacity-0"
-                >
-                    <div v-show="show" class="fixed inset-0 transform transition-all" @click="close">
-                        <div class="absolute inset-0 bg-gray-500 opacity-75" />
-                    </div>
-                </Transition>
-
-                <Transition
-                    enter-active-class="ease-out duration-300"
-                    enter-from-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                    enter-to-class="opacity-100 translate-y-0 sm:scale-100"
-                    leave-active-class="ease-in duration-200"
-                    leave-from-class="opacity-100 translate-y-0 sm:scale-100"
-                    leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                >
-                    <div
-                        v-show="show"
-                        class="mb-6 bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full sm:mx-auto"
-                        :class="maxWidthClass"
-                    >
-                        <slot v-if="show" />
-                    </div>
-                </Transition>
-            </div>
-        </Transition>
-    </Teleport>
+  <Teleport to="body">
+    <md-dialog
+      :id="id"
+      v-bind="$attrs"
+      ref="modal"
+    >
+      <slot />
+    </md-dialog>
+  </Teleport>
 </template>
+
+<style scoped>
+.dark md-dialog {
+  --md-dialog-container-color: theme(colors.dark.background);
+  --md-dialog-headline-color: theme(colors.dark.on-background);
+  --md-dialog-supporting-text-color: theme(colors.dark.on-background);
+}
+md-dialog {
+  --md-dialog-container-color: theme(colors.light.background);
+  --md-dialog-headline-color: theme(colors.light.on-background);
+  --md-dialog-supporting-text-color: theme(colors.light.on-background);
+}
+</style>
