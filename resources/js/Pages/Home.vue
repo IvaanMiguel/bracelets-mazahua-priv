ce9d1e4949a8115a681c0a5918fb7329302277ef
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-02-19 14:46:13 -0700

    Page templates placeholders creados

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import MainLayout from '@/Layouts/MainLayout.vue'
+
+defineOptions({
+  layout: MainLayout,
+})
+</script>
+
+<template>
+  <h1 class="text-2xl text-light-primary dark:text-dark-primary">
+    Home page
+  </h1>
+  {{ $page.component }}
+</template>
