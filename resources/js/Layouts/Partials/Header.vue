75a79b0a0274afaa007df8996c16b92a23ecae51
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-02-19 14:48:09 -0700

    Partial Header creado.

---
+++ resources/js/Layouts/Partials/Header.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import ColorMode from './ColorMode.vue'
+</script>
+
+<template>
+  <header
+    class="flex items-center justify-between gap-2 border-b-2 border-light-on-surface p-4 dark:border-dark-on-surface"
+  >
+    <h1
+      class="text-2xl font-medium text-light-primary dark:text-dark-on-surface"
+    >
+      Bracelets Mazahua
+    </h1>
+    <ColorMode class="ms-auto" />
+    <h6 class="text-lg text-light-on-surface dark:text-dark-on-surface">
+      {{ $page.props.auth.user.username }}
+    </h6>
+  </header>
+</template>
