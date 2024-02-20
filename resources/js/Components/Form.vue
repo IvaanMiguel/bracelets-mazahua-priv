17237c7e2c9084c8889029cb68a12967a15398e7
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-02-20 00:32:01 -0700

    Componente Form creado.

---
+++ resources/js/Components/Form.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue'
+
+const props = withDefaults(
+  defineProps<{
+    submit: () => void
+    notSubmitOnEnter?: boolean
+  }>(),
+  {
+    notSubmitOnEnter: false,
+  }
+)
+
+const form = ref<HTMLFormElement | null>(null)
+
+const submitFormOnEnter   = (e: KeyboardEvent) => {
+  if (e.code !== 'Enter') return
+
+  props.submit()
+}
+
+onMounted(() => {
+  if (props.notSubmitOnEnter) return
+
+  form.value?.addEventListener('keydown', submitFormOnEnter)
+})
+
+onUnmounted(() => {
+  form.value?.removeEventListener('keydown', submitFormOnEnter)
+})
+</script>
+
+<template>
+  <form
+    @submit.prevent="submit"
+    ref="form"
+  >
+    <slot />
+  </form>
+</template>
