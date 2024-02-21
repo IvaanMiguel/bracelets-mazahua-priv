9a989b26cfb52b45dc96b3c1ac2ccb776e4a7884
Marielena08 <marielenan_20@alu.uabcs.mx>
2024-02-21 09:26:16 -0700

    >Fuente roboto añadida.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,14 +26,26 @@
     href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:FILL@0..1"
     rel="stylesheet"
   >
-
+  <link
+    href="https://fonts.googleapis.com"
+    rel="preconnect"
+  >
+  <link
+    href="https://fonts.gstatic.com"
+    rel="preconnect"
+    crossorigin
+  >
+  <link
+    href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+    rel="stylesheet"
+  >
   <!-- Scripts -->
   @routes
   @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
   @inertiaHead
 </head>
 
-<body class='h-svh bg-light-background font-sans antialiased dark:bg-dark-background'>
+<body class='h-svh bg-light-background dark:bg-dark-background font-sans antialiased'>
   @inertia
 </body>
 
