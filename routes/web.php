050c2bf50370753898bfc0754e67147a3b5fe857
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-02-20 22:55:14 -0700

    Rutas index creadas.

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,33 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', HomeController::class)
+    ->middleware('auth')
+    ->name('home');
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+Route::controller(CustomerController::class)->prefix('customers')->group(function () {
+    Route::get('/', 'index')->name('customers');
+});
+
+Route::controller(ProductController::class)->prefix('products')->group(function () {
+    Route::get('/', 'index')->name('products');
+});
+
+Route::controller(CategoryController::class)->prefix('categories')->group(function () {
+    Route::get('/', 'index')->name('categories');
+});
+
+Route::controller(OrderController::class)->prefix('orders')->group(function () {
+    Route::get('/', 'index')->name('orders');
 });
 
 Route::get('/dashboard', function () {
