dce053900a214c52bbbb9c4e5fc02a38a7b7816f
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-02-23 22:37:44 -0700

    Template creado.

---
+++ resources/js/Pages/Customers/Show.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Customer } from '@/types/customers'
+
+import MainLayout from '@/Layouts/MainLayout.vue'
+
+defineOptions({
+  layout: MainLayout
+})
+
+defineProps<{
+  customer: Customer
+}>()
+</script>
+
+<template>
+  <p>{{ customer }}</p>
+</template>
