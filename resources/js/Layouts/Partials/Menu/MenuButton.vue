ecdc925434bbf6ed82d02289492461040484ae9f
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-02-23 22:54:00 -0700

    Prop active añadida.

--- resources/js/Layouts/Partials/Menu/MenuButton.vue
+++ resources/js/Layouts/Partials/Menu/MenuButton.vue
@@ -4,19 +4,29 @@ import '@material/web/focus/md-focus-ring'
 import '@material/web/ripple/ripple'
 import Icon from '@/Components/Icon.vue'
 
-defineProps<{
-  icon?: string
-}>()
+withDefaults(
+  defineProps<{
+    icon?: string
+    active?: boolean
+  }>(),
+  {
+    active: false
+  }
+)
 </script>
 
 <template>
   <component
     :is="$attrs.href ? 'Link' : 'button'"
     class="active:md-elevation-1 hover:md-elevation-4 relative flex items-center gap-3 rounded-full p-3 text-light-on-primary outline-none dark:text-dark-on-primary-container"
+    :class="{
+      'bg-light-on-primary !text-light-primary dark:bg-dark-on-primary-container dark:!text-dark-primary-container':
+        active,
+    }"
     as="button"
   >
     <span class="absolute left-0 right-0 top-1/2 h-12 -translate-y-1/2"></span>
-    <Icon>{{ icon }}</Icon>
+    <Icon :filled="active">{{ icon }}</Icon>
     <slot />
     <md-ripple aria-hidden="true" />
     <md-focus-ring aria-hidden="true" />
