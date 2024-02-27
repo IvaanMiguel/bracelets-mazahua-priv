c7f0b73850cda06ac94a04eec5f92a4d47ff8055
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-02-27 00:06:31 -0700

    Animaciones añadidas.

--- resources/js/Components/ColorMode.vue
+++ resources/js/Components/ColorMode.vue
@@ -1,19 +1,27 @@
 <script setup lang="ts">
 import Icon from '@/Components/Icon.vue'
 
-import { computed } from 'vue'
 import { useDark, useToggle } from '@vueuse/core'
 
 const isDark = useDark()
 const toggleDark = useToggle(isDark)
-
-const colorModeIcon = computed(() =>
-  isDark.value ? 'light_mode' : 'dark_mode'
-)
 </script>
 
 <template>
   <md-icon-button @click="toggleDark()">
-    <Icon>{{ colorModeIcon }}</Icon>
+    <div
+      class="absolute left-0 top-0 grid h-full w-full place-content-center overflow-hidden rounded-full"
+    >
+      <Transition
+        mode="out-in"
+        enter-from-class="translate-y-7"
+        enter-active-class="transition-transform duration-200 ease-out"
+        leave-active-class="transition-transform duration-200 ease-out"
+        leave-to-class="-translate-y-7"
+      >
+        <Icon v-if="isDark">dark_mode</Icon>
+        <Icon v-else>light_mode</Icon>
+      </Transition>
+    </div>
   </md-icon-button>
 </template>
