70d703cfd6972072d0aace2e175fbe7f8efd2087
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-02-27 00:20:30 -0700

    Template añadido.

---
+++ resources/js/Pages/Customers/Partials/Create.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import '@material/web/button/filled-button'
+import '@material/web/divider/divider'
+import '@material/web/elevation/elevation'
+
+import { useForm } from '@inertiajs/vue3'
+
+import { ref } from 'vue'
+import PersonalInfoForm from './PersonalInfoForm.vue'
+import Icon from '@/Components/Icon.vue'
+import Snackbar from '@/Components/Snackbar.vue'
+
+const successSnackbar = ref<typeof Snackbar | null>(null)
+
+const form = useForm({
+  name: '',
+  last_name: '',
+  birth_date: '',
+  email: '',
+  phone_number: '',
+  // main_street: '',
+  // cross_streets: '',
+  // neighborhood: '',
+  // street_number: '',
+  // suite_number: '',
+  // postal_code: '',
+})
+
+const submit = () => {
+  form.post(route('customers.store'), {
+    onSuccess: () => {
+      form.reset()
+      successSnackbar.value?.show()
+    }
+  })
+}
+</script>
+
+<template>
+  <div
+    class="md-elevation-1 relative rounded-lg bg-light-surface-container-lowest p-4 dark:bg-dark-surface-container"
+  >
+    <md-elevation />
+    <PersonalInfoForm
+      :submit="submit"
+      :form="form"
+    />
+    <md-divider class="my-4" />
+    <h1
+      class="mb-4 text-xl font-medium text-light-on-background dark:text-dark-on-background"
+    >
+      Direcciones
+    </h1>
+    <div class="text-end">
+      <md-filled-button
+        @click="submit"
+        :disabled="form.processing"
+      >
+        Guardar cliente
+        <Icon slot="icon">save</Icon>
+      </md-filled-button>
+    </div>
+    <Snackbar ref="successSnackbar" close-button>
+      <template #content>
+        Información del cliente guardada con éxito.
+      </template>
+    </Snackbar>
+  </div>
+</template>
