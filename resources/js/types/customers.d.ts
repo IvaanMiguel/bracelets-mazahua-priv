2e12770d0951d3e65ebf0ffab3fb20e14ad5bac7
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-02-27 22:54:19 -0700

    Interfaz Address añadida.

--- resources/js/types/customers.d.ts
+++ resources/js/types/customers.d.ts
@@ -7,7 +7,16 @@ export interface CustomerListItem {
 export interface Customer {
   name: string,
   last_name: string
-  birth_date: string
-  email: string
+  birth_date: string | null
+  email: string | null
   phone_number: string
+}
+
+export interface Address {
+  main_street: string,
+  cross_streets: string | null
+  neighborhood: string
+  street_number: string | null
+  suite_number: string | null
+  postal_code: string
 }
