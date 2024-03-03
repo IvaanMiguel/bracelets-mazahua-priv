7aa4344dd97ebc9b50d4f23e91150f074d30ae58
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-03-03 12:57:24 -0700

    Comportamiento de MdDialog apilado solucionado

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -1,13 +1,42 @@
+// @ts-nocheck
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import axios from 'axios';
-window.axios = axios;
+import { MdDialog } from '@material/web/dialog/dialog.js'
+import axios from 'axios'
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios = axios
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+
+/**
+ * Material web components workaround to issue No. 5500.
+ * https://github.com/material-components/material-web/issues/5500
+ */
+
+MdDialog.addInitializer(async (instance: MdDialog) => {
+  await instance.updateComplete
+  instance.dialog.prepend(instance.scrim)
+  instance.scrim.style.inset = 0
+  instance.scrim.style.zIndex = -1
+
+  const { getOpenAnimation, getCloseAnimation } = instance
+  instance.getOpenAnimation = () => {
+    const animations = getOpenAnimation.call(this)
+    animations.container = [...animations.container, ...animations.dialog]
+    animations.dialog = []
+    return animations
+  }
+  instance.getCloseAnimation = () => {
+    const animations = getCloseAnimation.call(this)
+    animations.container = [...animations.container, ...animations.dialog]
+    animations.dialog = []
+    return animations
+  }
+})
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
