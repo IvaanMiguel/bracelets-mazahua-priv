586a60c2223ea8665c3a516e240d950adc5b9a5c
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-03-03 14:31:13 -0700

    Watch a errores de página añadido.

--- resources/js/composables/useFormErrors.ts
+++ resources/js/composables/useFormErrors.ts
@@ -1,23 +1,25 @@
-import { InertiaForm } from '@inertiajs/vue3'
-import { GlobalConfig, ValidationArgs } from '@vuelidate/core'
-import useVuelidate from '@vuelidate/core'
+import { InertiaForm, usePage } from '@inertiajs/vue3'
+import useVuelidate, { GlobalConfig, ValidationArgs } from '@vuelidate/core'
 import { watch } from 'vue'
 
 export function useFormErrors<
   T extends { [key in keyof ValidationArgs]: any },
   F extends { [key in keyof T]: any },
->(
-  rules: T,
-  form: InertiaForm<F>,
-  globalConfig?: GlobalConfig
-) {
+>(rules: T, form: InertiaForm<F>, globalConfig?: GlobalConfig) {
   const v$ = useVuelidate(rules, form, globalConfig)
+  const page = usePage()
 
-  watch(v$, () => {
+  watch(() => v$.value.$errors, () => {
     for (const key in form.data()) {
       form.setError(key, v$.value[key].$errors[0]?.$message)
     }
   })
 
-  return { v$ }
+  watch(() => page.props.errors, () => {
+    for (const key in form.data()) {
+      form.setError(key, page.props.errors[key])
+    }
+  })
+
+  return { v$, page }
 }
