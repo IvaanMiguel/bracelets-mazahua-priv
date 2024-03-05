2f389a07d33dad2c7d23987790c9a8c8f5ab3d33
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-03-04 19:31:02 -0700

    Archivos y rutas innecesarias eliminadas.

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,8 @@ use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', HomeController::class)
     ->middleware('auth')
@@ -30,16 +28,6 @@ Route::controller(CategoryController::class)->prefix('categories')->group(functi
 
 Route::controller(OrderController::class)->prefix('orders')->group(function () {
     Route::get('/', 'index')->name('orders');
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
