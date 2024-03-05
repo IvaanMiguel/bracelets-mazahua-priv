c03f162a21857011d04d3004a6a680e3e60d8cc0
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-03-04 21:11:08 -0700

    Traducción al español añadida.

---
+++ lang/es/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'failed'   => 'Los datos ingresados no coinciden.',
+    'password' => 'La contraseña es incorrecta.',
+    'throttle' => 'Demasiados intentos de acceso. Por favor intente nuevamente en :seconds segundos.',
+];
