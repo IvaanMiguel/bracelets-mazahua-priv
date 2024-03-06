605f0387efdf00ab4d1fabfb429c13dc5cfe7322
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-03-06 00:21:58 -0700

    Componente DisplayField creado.

---
+++ resources/js/Components/DisplayField.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  label: string
+  text: string
+}>()
+</script>
+
+<template>
+  <div class="flex gap-4">
+    <slot name="icon" />
+    <div class="text-on-background flex flex-col gap-2 overflow-hidden">
+      <div class="truncate text-xs font-medium">{{ label }}</div>
+      <div class="truncate text-base">{{ text }}</div>
+    </div>
+  </div>
+</template>
