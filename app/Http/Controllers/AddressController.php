99094a38804d089c809f3a94904ef94e6a73691f
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-03-06 18:36:52 -0700

    AddressController creado.

---
+++ app/Http/Controllers/AddressController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Address;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+
+class AddressController extends Controller
+{
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        try {
+            $address = Address::findOrFail($id);
+        } catch (ModelNotFoundException $modelNotFoundException) {
+            return back()->withErrors([
+                'internal_error' => 'Ha ocurrido un error al eliminar la dirección seleccionada.'
+            ]);
+        }
+
+        if (Address::where('customer_id', '=', $address->customer_id)->count() <= 1) {
+            return back()->withErrors([
+                'internal_error' => 'No es posible eliminar la única dirección del cliente.'
+            ]);
+        }
+
+        $address->delete();
+
+        return back();
+    }
+}
