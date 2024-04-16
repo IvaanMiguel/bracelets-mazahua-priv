a358d29715fe4fadbfca904f4ebceae96866aba7
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-04-16 01:22:16 -0700

    Color de fondo modificado.

--- resources/js/Components/Modal.vue
+++ resources/js/Components/Modal.vue
@@ -28,3 +28,16 @@ defineExpose({ dialog })
     </md-dialog>
   </Teleport>
 </template>
+
+<style scoped>
+.dark md-dialog {
+  --md-dialog-container-color: theme(colors.dark.background);
+  --md-dialog-headline-color: theme(colors.dark.on-background);
+  /* --md-dialog-supporting-text-color: theme(colors.dark.on-background); */
+}
+md-dialog {
+  --md-dialog-container-color: theme(colors.light.background);
+  --md-dialog-headline-color: theme(colors.light.on-background);
+  /* --md-dialog-supporting-text-color: theme(colors.light.on-background); */
+}
+</style>
