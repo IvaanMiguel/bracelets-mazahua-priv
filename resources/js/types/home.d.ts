910b7090d9b3235ea83f2757e98aff22c830dda1
Marielena08 <marielenan_20@alu.uabcs.mx>
2024-04-23 09:58:35 -0700

    Interfaz SalesPerCategory añadida.

--- resources/js/types/home.d.ts
+++ resources/js/types/home.d.ts
@@ -1,4 +1,10 @@
-export interface completedOrder {
+export interface CompletedOrder {
   name: string
   total: number
+}
+
+export interface SalesPerCategory {
+  id: number
+  name: string
+  total_sales: string | number
 }
