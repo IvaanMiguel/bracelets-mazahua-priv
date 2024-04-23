233afc541608f45f3025b573ecadf8294b0f75eb
Marielena08 <marielenan_20@alu.uabcs.mx>
2024-04-23 10:00:23 -0700

    Componente SalesPerCategoryChart implementado.

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
 import MainLayout from '@/Layouts/MainLayout.vue'
 import '@material/web/elevation/elevation'
+import { Chart, Tooltip } from 'chart.js'
 import CompletedOrdersChart from './Partials/CompletedOrdersChart.vue'
+import SalesPerCategoryChart from './Partials/SalesPerCategoryChart.vue'
+
+Chart.register({
+  Tooltip,
+})
 
 defineOptions({
   layout: MainLayout,
@@ -9,12 +15,13 @@ defineOptions({
 </script>
 
 <template>
-  <div class="p-4 h-full">
+  <div class="h-full p-4">
     <div
-      class="md-elevation-1 relative mx-auto grid w-full max-w-6xl grid-cols-1 grid-rows-4 rounded-lg bg-light-surface-container-lowest p-4 dark:bg-dark-surface-container sm:grid-cols-2 md:grid-cols-4 h-full"
+      class="md-elevation-1 relative mx-auto grid h-full w-full grid-cols-1 grid-rows-4 rounded-lg bg-light-surface-container-lowest p-4 dark:bg-dark-surface-container sm:grid-cols-2 md:grid-cols-4"
     >
       <md-elevation />
       <CompletedOrdersChart class="row-span-2" />
+      <SalesPerCategoryChart class="col-span-2 col-start-3 row-span-4" />
     </div>
   </div>
 </template>
