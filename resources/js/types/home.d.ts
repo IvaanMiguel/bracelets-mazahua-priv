112bdf3d144e4ac632e3c58a842da88b231fa843
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-04-24 16:36:23 -0700

    Interfaz BestSeller implementada.

--- resources/js/types/home.d.ts
+++ resources/js/types/home.d.ts
@@ -8,3 +8,10 @@ export interface SalesPerCategory {
   name: string
   total_sales: string | number
 }
+
+export interface BestSeller {
+  product_id: number
+  name: string
+  total_sales: string | number
+  units: string | number
+}
