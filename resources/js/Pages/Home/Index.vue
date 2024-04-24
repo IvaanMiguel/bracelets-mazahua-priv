a45ef6272e9676a6f960f5f9c2b918874ed188ac
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-04-24 16:38:18 -0700

    Componente BestSellerChart implementado.

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -3,6 +3,7 @@ import MainLayout from '@/Layouts/MainLayout.vue'
 import '@material/web/elevation/elevation'
 import { Chart, SubTitle, Title, Tooltip } from 'chart.js'
 import ChartDataLabels from 'chartjs-plugin-datalabels'
+import BestSellerChart from './Partials/BestSellerChart.vue'
 import CompletedOrdersChart from './Partials/CompletedOrdersChart.vue'
 import SalesPerCategoryChart from './Partials/SalesPerCategoryChart.vue'
 
@@ -21,11 +22,12 @@ defineOptions({
 <template>
   <div class="p-4">
     <div
-      class="md-elevation-1 relative mx-auto grid h-full w-full grid-cols-1 grid-rows-[repeat(7,160px)] rounded-lg bg-light-surface-container-lowest p-4 dark:bg-dark-surface-container sm:grid-cols-2 md:grid-cols-4"
+      class="md-elevation-1 relative mx-auto grid h-full w-full grid-cols-1 grid-rows-[repeat(7,160px)] gap-1 rounded-lg bg-light-surface-container-lowest p-4 dark:bg-dark-surface-container md:grid-cols-2 lg:grid-cols-4"
     >
       <md-elevation />
       <CompletedOrdersChart class="row-span-2" />
-      <SalesPerCategoryChart class="col-span-2 col-start-3 row-span-4" />
+      <BestSellerChart />
+      <SalesPerCategoryChart class="row-span-3 md:col-span-2 md:row-span-4" />
     </div>
   </div>
 </template>
---
+++ resources/js/Pages/Home/Partials/BestSellerChart.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Icon from '@/Components/Icon.vue'
+import { BestSeller } from '@/types/home'
+import { usePage } from '@inertiajs/vue3'
+import { ref } from 'vue'
+
+const page = usePage()
+const bestSeller = ref(page.props.bestSeller as BestSeller)
+</script>
+
+<template>
+  <div
+    class="flex flex-col items-center justify-center rounded-md border border-light-outline-variant p-2 text-center dark:border-dark-outline-variant text-on-background"
+  >
+    <Icon size="2.5rem">sell</Icon>
+    <span class="text-xs">Producto más vendido</span>
+    <span class="text-lg font-medium">{{ bestSeller.name }}</span>
+    <span class="text-xs">
+      {{ `${bestSeller.units} unidades vendidas en` }}
+    </span>
+    <span class="text-lg font-medium">
+      {{ `$${bestSeller.total_sales} MXN` }}
+    </span>
+  </div>
+</template>
