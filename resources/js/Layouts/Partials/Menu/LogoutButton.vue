c642513993a2a7554589f65397a122688c9f6ce8
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-04-27 22:29:31 -0700

    useModal removido, botones deshabilitados y modal no cancelable implementado.

--- resources/js/Layouts/Partials/Menu/LogoutButton.vue
+++ resources/js/Layouts/Partials/Menu/LogoutButton.vue
@@ -1,38 +1,60 @@
 <script setup lang="ts">
 import Icon from '@/Components/Icon.vue'
 import Modal from '@/Components/Modal.vue'
-import { useModal } from '@/composables/useModal'
+import { router } from '@inertiajs/vue3'
 import '@material/web/button/text-button'
+import { ref } from 'vue'
 import MenuButton from './MenuButton.vue'
 
 defineProps<{ isExpanded: boolean }>()
 
-const { modal } = useModal('#logout-modal')
+const processing = ref(false)
+const modal = ref<InstanceType<typeof Modal>>()
+
+const logout = () => {
+  processing.value = true
+
+  router.post(
+    route('logout'),
+    {},
+    {
+      onFinish: () => {
+        processing.value = false
+      },
+    }
+  )
+}
 </script>
 
 <template>
   <MenuButton
     class="mt-auto"
     icon="logout"
-    @click="modal?.show()"
+    @click="modal?.dialog?.show()"
   >
     <template v-if="isExpanded">Cerrar sesión</template>
   </MenuButton>
 
-  <Modal id="logout-modal">
+  <Modal
+    ref="modal"
+    :not-cancellable="processing"
+  >
     <Icon slot="icon">logout</Icon>
     <div slot="headline">Cerrar sesión</div>
     <div slot="content">¿Deseas cerrar la sesión actual?</div>
     <div slot="actions">
-      <md-text-button @click="modal?.close()">Cancelar</md-text-button>
-      <Link
-        tabindex="-1"
-        :href="route('logout')"
-        method="POST"
-        as="button"
+      <md-text-button
+        @click="modal?.dialog?.close()"
+        :disabled="processing"
       >
-        <md-text-button>Aceptar</md-text-button>
-      </Link>
+        Cancelar
+      </md-text-button>
+      <md-text-button
+        @click="logout"
+        :disabled="processing"
+      >
+        Aceptar
+      </md-text-button>
     </div>
   </Modal>
 </template>
