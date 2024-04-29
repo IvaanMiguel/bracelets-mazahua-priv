5165c61586346fc497e770f60c797d371ef95ab7
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-04-28 19:22:58 -0700

    CustomersList reemplazado por vista Index.

--- resources/js/Pages/Customers/Index.vue
+++ resources/js/Pages/Customers/Index.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+import HighlightSearch from '@/Components/HighlightSearch.vue'
 import Icon from '@/Components/Icon.vue'
+import SearchBar from '@/Components/SearchBar.vue'
 import Snackbar from '@/Components/Snackbar.vue'
-import Tabs from '@/Components/Tabs.vue'
+import Table from '@/Components/Table.vue'
 import MainLayout from '@/Layouts/MainLayout.vue'
 import { Pagination } from '@/types'
 import { CustomerListItem } from '@/types/customers'
+import { router } from '@inertiajs/vue3'
 import '@material/web/button/filled-button'
 import '@material/web/button/filled-tonal-button'
 import '@material/web/divider/divider'
@@ -14,8 +17,6 @@ import '@material/web/list/list'
 import '@material/web/list/list-item'
 import '@material/web/tabs/primary-tab'
 import { onMounted, ref } from 'vue'
-import Create from './Partials/Create.vue'
-import CustomersList from './Partials/CustomersList.vue'
 
 defineOptions({ layout: MainLayout })
 const props = defineProps<{
@@ -26,6 +27,8 @@ const props = defineProps<{
 const destroyedCustomerSnackbar = ref<InstanceType<typeof Snackbar>>()
 
 onMounted(() => {
+  router.reload({ only: ['customers', 'destroyed'] })
+
   if (!props.destroyed) return
 
   destroyedCustomerSnackbar.value?.show(true)
@@ -33,51 +36,70 @@ onMounted(() => {
 </script>
 
 <template>
-  <Tabs auto-activate>
-    <template #tab>
-      <md-primary-tab
-        id="list-tab"
-        aria-controls="list-panel"
-      >
-        <Icon slot="icon">list_alt</Icon>
-        Clientes
-      </md-primary-tab>
-      <md-primary-tab
-        id="create-tab"
-        aria-controls="create-panel"
-      >
-        <Icon slot="icon">add</Icon>
-        Agregar cliente nuevo
-      </md-primary-tab>
-    </template>
-
-    <template #panel>
-      <div
-        id="list-panel"
-        class="overflow-y-auto p-4"
-        aria-labelledby="list-tab"
-        role="tabpanel"
-      >
-        <CustomersList
-          class="mx-auto w-full max-w-6xl"
-          :customers="customers"
+  <div class="h-full p-4">
+    <div
+      class="md-elevation-1 relative mx-auto flex h-full w-full max-w-6xl flex-col rounded-lg bg-light-surface-container-lowest dark:bg-dark-surface-container"
+    >
+      <md-elevation />
+      <div class="flex items-center justify-between gap-4 p-4 pb-2">
+        <h1
+          class="text-2xl font-medium text-light-on-background dark:text-dark-on-background"
+        >
+          {{ `${customers.total} cliente${customers.total !== 1 ? 's' : ''}` }}
+        </h1>
+        <SearchBar
+          class="ms-auto w-full max-w-96"
+          placeholder="Buscar..."
+          :base-url="route('customers')"
         />
 
-        <Snackbar
-          ref="destroyedCustomerSnackbar"
-          text="Cliente eliminado con éxito."
-          close-button
-        />
+        <Link
+          :href="route('customers.create')"
+          tabindex="-1"
+        >
+          <md-filled-button>
+            <Icon slot="icon">add</Icon>
+            Agregar
+          </md-filled-button>
+        </Link>
       </div>
-
-      <div
-        id="create-panel"
-        class="overflow-y-auto p-4"
-        aria-labelledby="create-tab"
-        role="tabpanel"
+      <Table
+        class="flex h-full flex-col overflow-hidden"
+        :headers="['Nombre(s)', 'Apellido(s)', 'Número celular']"
+        :pagination="customers"
+        :url="route('customers')"
       >
-        <Create class="mx-auto w-full max-w-6xl" />
-      </div>
-    </template>
-  </Tabs>
+        <template v-for="(customer, i) in customers.data">
+          <Link
+            :href="route('customers.show', { id: customer.id })"
+            tabindex="-1"
+          >
+            <md-list-item type="button">
+              <div class="grid grid-cols-3 gap-4">
+                <HighlightSearch
+                  class="truncate"
+                  :text="`${customer.name}`"
+                  :search="`${$page.props.filters.search}`"
+                />
+                <HighlightSearch
+                  class="truncate"
+                  :text="`${customer.last_name}`"
+                  :search="`${$page.props.filters.search}`"
+                />
+                <HighlightSearch
+                  class="truncate"
+                  :text="`${customer.phone_number}`"
+                  :search="`${$page.props.filters.search}`"
+                />
+              </div>
+            </md-list-item>
+            <md-divider
+              v-if="i !== customers.data.length - 1"
+              inset
+            />
+          </Link>
+        </template>
+      </Table>
+    </div>
+  </div>
 </template>
